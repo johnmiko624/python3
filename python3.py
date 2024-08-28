2a1aa384b40c139e5d5dd1856603416d183f73e1
johnmiko624 <palconitjohnmiko@gmail.com>
2024-08-28 17:51:32 +0800

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,63 @@
+Last login: Wed Aug 28 17:24:43 on ttys039
+johnmikosarsalijo@johns-MacBook-Air ~ % python3
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> first_name="""o     o             o   o   o        
+...     |     |             |\ /| o | /      
+...     | o-o O--o o-o      | O |   OO   o-o 
+... \   o | | |  | |  |     |   | | | \  | | 
+...  o-o  o-o o  o o  o     o   o | o  o o-o"""
+<stdin>:1: SyntaxWarning: invalid escape sequence '\ '
+>>> print(first_name)
+o     o             o   o   o        
+    |     |             |\ /| o | /      
+    | o-o O--o o-o      | O |   OO   o-o 
+\   o | | |  | |  |     |   | | | \  | | 
+ o-o  o-o o  o o  o     o   o | o  o o-o
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
